b2573cc05632a8a7cea64657c30b6bd137b76d4e
duram21 <cubeenjoy@naver.com>
2025-04-21 11:38:58 +0900

    Item UI 생성 및 버튼 이벤트 생성

---
+++ Assets/Script/Item2.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item2 : MonoBehaviour
+{
+    public ItemData data;
+    public int level;
+    public Weapon weapon;
+
+    Image icon;
+    Text textLevel;
+
+    void Awake()
+    {
+        icon = GetComponentsInChildren<Image>()[1];
+        icon.sprite = data.itemIcon;
+
+        Text[] texts = GetComponentsInChildren<Text>();
+        textLevel = texts[0];
+    }
+
+    void LateUpdate()
+    {
+        textLevel.text = "Lv." + (level + 1);
+    }
+
+    public void OnClick()
+    {
+        switch(data.itemType)
+        {
+            case ItemData.ItemType.Melee:
+
+            case ItemData.ItemType.Range:
+
+                break;
+            case ItemData.ItemType.Glove:
+
+                break;
+            case ItemData.ItemType.Shoe:
+
+                break;
+            case ItemData.ItemType.Heal:
+
+                break;
+        }
+
+        level++;
+
+        if(level == data.damages.Length)
+        {
+            GetComponent<Button>().interactable = false;
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

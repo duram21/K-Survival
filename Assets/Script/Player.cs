46ac6d4a2e2a514c1fd931d3bd726c1f00e92d61
duram21 <cubeenjoy@naver.com>
2025-04-22 09:47:19 +0900

    플레이어 피격 구현

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -44,10 +44,28 @@ public class Player : MonoBehaviour
     {
         if(!GameManager.instance.isLive)
             return;
-            
+
         anim.SetFloat("Speed", inputVec.magnitude);
 
         if(inputVec.x != 0)
             spriter.flipX = inputVec.x < 0; 
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(!GameManager.instance.isLive)
+            return;
+        
+        GameManager.instance.health -= Time.deltaTime * 10;
+
+        if (GameManager.instance.health  < 0)
+        {
+            for(int index  = 2; index < transform.childCount; index++)
+            {
+                transform.GetChild(index).gameObject.SetActive(false);
+            }
+
+            anim.SetTrigger("Dead");
+        }
     }
 }
